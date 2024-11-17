a513bc2721d827da044bd289cc181bf1dd4a42b3
hitesh-jatav <hkjatav@gmail.com>
2024-11-17 13:25:08 +0530

    calendar changes

--- src/Components/CalendarComponent.jsx
+++ src/Components/CalendarComponent.jsx
@@ -1,53 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css'; // Import the CSS for styling
 const localizer = momentLocalizer(moment);
 
-const events = [
-    {
-      title: 'Meeting',
-      start: new Date(2024, 10, 15, 10, 0),
-      end: new Date(2024, 10, 15, 12, 0),
-    },
-    {
-      title: 'Conference',
-      start: new Date(2024, 10, 16),
-      end: new Date(2024, 10, 17),
-    },
-    {
-      title: 'Lunch Break',
-      start: new Date(2024, 10, 17, 13, 0),
-      end: new Date(2024, 10, 17, 14, 0),
-    },
-  ];
 
-const CalendarComponent = () => {
-    const [calendarEvents, setCalendarEvents] = useState(events);
-    const dayStyleGetter = (date) => {
-        const today = moment().isSame(date, 'day');
-        return {
-          style: {
-            color: today ? 'coral' : '#000', // Highlight today's date in red
-            fontWeight: today ? 'bold' : 'normal',
-          },
-        };
-      };
+const CalendarComponent = ({ eventsList = [] }) => {
+  // styles the date-tile;
+  const dayStyleGetter = (date) => {
+    const today = moment().isSame(date, 'day');
+    const hasEvent = eventsList.some(event =>
+      moment(date).isBetween(event.start, event.end, 'day', '[]')
+    );
 
-    return (
-        <div>
-            <BigCalendar
-                localizer={localizer}
-                events={calendarEvents}
-                startAccessor="start"
-                endAccessor="end"
-                style={{ height: 500 }}
-                views={['month', 'week', 'day']}
-                defaultView="month"
-                dayPropGetter={dayStyleGetter}
-            />
-        </div>
-    )
-}
+    return {
+      style: {
+        color: today ? 'coral' : '#000',
+        fontWeight: today ? 'bold' : 'normal',
+        // border: hasEvent ? '1px solid green' : ''
+        // backgroundColor: hasEvent ? 'green' : 'transparent', // Apply background color for days with events
+      },
+    };
+  };
 
-export default CalendarComponent
+  return (
+    <div>
+      <BigCalendar
+        localizer={localizer}
+        events={eventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        views={['month', 'week', 'day']}
+        defaultView="month"
+        dayPropGetter={dayStyleGetter}
+      />
+    </div>
+  );
+};
+
+export default CalendarComponent;
--- src/Page/Calender.js
+++ src/Page/Calender.js
@@ -1,20 +1,54 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import btn from '../assets/images/add-btn-2.svg';
 import Heading from '../Components/Heading';
 import BookingBlock from '../Components/BookingBlock';
 import CalendarComponent from '../Components/CalendarComponent';
+import moment from "moment"
 
 
 
-const bookingData = [
-  { title: 'Employees', count: 200, icon: 'icon-employees', link: '#' },
-  { title: 'Non Employees', count: 160, icon: 'icon-employees', link: '#' },
-  { title: 'Buffer', count: 180, icon: 'icon-buffer', link: '#' },
+const getTitleString = (employee, nonEmployee, others) => {
+  let string = '';
+  if (employee)
+    string = string + `Emp: ${employee}, `
+
+  if (nonEmployee)
+    string = string + ` Non-Emp: ${nonEmployee}, `
+
+  if (others)
+    string = string + ` Others: ${others}`
+
+  return string.trim();
+}
+
+const events = [
+  {
+    title: getTitleString(3, 4, 5),
+    start: new Date(2024, 10, 12, 10, 0),
+    end: new Date(2024, 10, 12, 12, 0),
+  },
+  {
+    title: getTitleString(24, 0, 2),
+    start: new Date(2024, 10, 14, 16),
+    end: new Date(2024, 10, 14, 17),
+  },
+  {
+    title: getTitleString(1, 3, 0),
+    start: new Date(2024, 10, 17, 13, 0),
+    end: new Date(2024, 10, 17, 14, 0),
+  },
 ];
 
 const Calendar = () => {
 
+  let currentDateBookings = [33, 254, 22];
+
+  const bookingData = [
+    { title: 'Employees', count: currentDateBookings[0], icon: 'icon-employees', link: '#' },
+    { title: 'Non Employees', count: currentDateBookings[1], icon: 'icon-employees', link: '#' },
+    { title: 'Buffer', count: currentDateBookings[2], icon: 'icon-buffer', link: '#' },
+  ];
 
   return (
     <div className="container-fluid top-80">
@@ -24,12 +58,12 @@ const Calendar = () => {
           <div className="row">
             <div className="col-lg-9">
               <div className="tile">
-                <CalendarComponent/>     
+                <CalendarComponent eventsList={events} />
               </div>
             </div>
             <div className="col-lg-3">
               <div className="tile">
-                <Heading title="Saturday, 19 Dec 2022" classes="tile-title" />
+                <Heading title={moment().format('dddd, D MMM YYYY')} classes="tile-title" />
                 <div className="booking-wrapper">
                   <div className="booking-block" style={{ backgroundColor: 'Blue' }}>
                     <h5>
